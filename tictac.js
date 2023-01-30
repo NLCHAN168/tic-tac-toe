00d8c4b57d613d4a58c0c10dfcfb8a0095ced5b4
Nick <nlchan168@yahoo.com>
2023-01-30 05:06:58 -0500

    fixed some code structure, next fix will be win logic

--- tictac.js
+++ tictac.js
@@ -10,6 +10,7 @@ const gameBoard = () => {
   const printBoard = () => {
     console.table(board);
   };
+
   const arrayCopy = () => board.map((obj) => ({ ...obj }));
 
   return { arrayCopy, printBoard, place };
@@ -27,13 +28,28 @@ const Player = function (number, sign) {
 
 const Game = () => {
   let game = gameBoard();
+  let newBoard = game.arrayCopy();
   const display = () => {
     game.printBoard();
   };
+
+  const move = (ind1, ind2, playerSymbol) => {
+    game.place(ind1, ind2, playerSymbol);
+    newBoard = game.arrayCopy();
+  };
+
+  const checkWin = (playerSymbol) => {
+    console.log("[0][0]:" + newBoard[0][0]);
+    checkRow(playerSymbol);
+    checkColumn(playerSymbol);
+    checkDiag(playerSymbol);
+    checkAntiDiag(playerSymbol);
+  };
+
   const checkRow = (playerSymbol) => {
     for (let row = 0; row < 3; row++) {
       for (let column = 2; column >= 0; column--) {
-        if (playerSymbol != game[row][column]) {
+        if (playerSymbol != newBoard[row][column]) {
           break;
         } else {
           console.log("Win!");
@@ -45,7 +61,7 @@ const Game = () => {
   const checkColumn = (playerSymbol) => {
     for (let column = 0; column < 3; column++) {
       for (let row = 0; row < 3; row++) {
-        if (playerSymbol != game[column][row]) {
+        if (playerSymbol != newBoard[column][row]) {
           break;
         } else {
           console.log("Win!");
@@ -56,7 +72,7 @@ const Game = () => {
 
   const checkDiag = (playerSymbol) => {
     for (let i = 0; i < 3; i++) {
-      if (playerSymbol != game[i][i]) {
+      if (playerSymbol != newBoard[i][i]) {
         break;
       } else {
         console.log("Win!");
@@ -65,15 +81,25 @@ const Game = () => {
   };
 
   const checkAntiDiag = (playerSymbol) => {
-    //some code
+    let i = 2;
+    let j = 0;
+    while (playerSymbol == newBoard[i][j] && i >= 0 && j < 3) {
+      i--;
+      j++;
+      if (i === 0 && j === 2) {
+        console.log("Win!");
+      }
+    }
   };
 
-  game.place(0, 1, player1.getSign());
-  game.place(0, 2, player2.getSign());
-  return { display, checkRow, checkColumn, checkDiag, checkAntiDiag };
+  return { display, move, checkWin };
 };
 
 let player1 = Player("1", "x");
 let player2 = Player("2", "O");
 newGame = Game();
+newGame.move(0, 0, player1.getSign());
+newGame.move(0, 1, player1.getSign());
+newGame.move(0, 2, player1.getSign());
 newGame.display();
+newGame.checkWin(player1.getSign());
